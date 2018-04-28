fe7c496f0965c008e8fe1efbb485623e0d410878
Victor <doc99tor@gmail.com>
2018-04-28 21:27:39 +0300

    if, switch and function

---
+++ main.js
@@ -0,0 +1,26 @@
+var a = 5;
+var b = 15;
+console.log(a);
+
+if (a % 2 === 1) {
+	console.log('odd');
+} else {
+	console.log('even');
+}
+
+printDigitName(a);
+printDigitName(b);
+printDigitName(77);
+printDigitName(3.5);
+
+function printDigitName(digit) {
+	var digitName = '';
+	switch (digit) {
+		case 1: digitName = 'one'; break;
+		case 2: digitName = 'two'; break;
+		case 3: digitName = 'three'; break;
+		case 5: digitName = 'five'; break;
+		case 15: digitName = 'fifteen'; break;
+	}
+	console.log(digitName);
+}
